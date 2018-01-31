2ba758b2ed3a5192ddb0adbe60c41eaa89bdeb17
AWS2-05 <mcolominasrojas@iesesteveterradas.cat>
2018-01-31 20:12:48 +0100

    commit

--- votaSite/vota/admin.py
+++ votaSite/vota/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Encuesta, AccesoEncuesta
+from .models import Encuesta, Opcion, Voto
 
-admin.site.register(Encuesta)
-admin.site.register(AccesoEncuesta)
+class OpcionesEnLinea(admin.StackedInline):
+	model = Opcion
+	extra = 2
+
+class EncuestaAdmin(admin.ModelAdmin):
+	def get_queryset(self, request):
+		qs = super().get_queryset(request)
+		if request.user.is_superuser:
+			return qs
+		return qs.filter(usuario=request.user)
+
+	list_display = ('nombre', 'activo')
+	inlines = [OpcionesEnLinea]
+
+class OpcionAdmin(admin.ModelAdmin):
+	def get_queryset(self, request):
+		qs = super().get_queryset(request)
+		if request.user.is_superuser:
+			return qs
+		return qs.filter(encuesta__usuario=request.user)
+
+	list_display = ('nombre', 'encuesta', 'votos_Totales')
+	ordering = ['-encuesta']
+
+admin.site.register(Encuesta, EncuestaAdmin)
+admin.site.register(Opcion, OpcionAdmin)
+admin.site.register(Voto)
--- votaSite/vota/models.py
+++ votaSite/vota/models.py
@@ -1,23 +1,35 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Encuesta(models.Model):
 	usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=100)
 	multirespuesta = models.BooleanField()
-	descripcion = models.CharField(max_length=200)
+	descripcion = models.CharField(max_length=200, blank=True, null=True)
 	inicio = models.DateTimeField('Data de publicacion')
-	fin = models.DateTimeField('Data de cerrar')
+	fin = models.DateTimeField('Data de expiracion')
 
-class OpcionesEncuestas(models.Model):
+	def activo(self):
+		return self.inicio <= timezone.now() and self.fin >= timezone.now()
+	activo.boolean = True
+
+	def __str__(self):
+		return self.nombre
+
+class Opcion(models.Model):
 	encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=100)
+	def votos_Totales(self):
+		return Voto.objects.filter(opcion=self).count()
+	def __str__(self):
+		return self.nombre
 
-class VotosEncuestas(models.Model):
+class Voto(models.Model):
 	usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	opcion = models.ForeignKey(OpcionesEncuestas, on_delete=models.CASCADE)
+	opcion = models.ForeignKey(Opcion, on_delete=models.CASCADE)
 
-class AccesoEncuesta(models.Model):
+class Acceso(models.Model):
 	usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
